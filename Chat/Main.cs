3d104a0de85f35d5089c0855c417ff53453ad5f1
faheem.u <faheem.fu@gmail.com>
2016-07-01 15:24:52 +0530

    Xamarin Chat IOS app Firebase
    A simple chat app for IOS , done in Xamarin with Firebase server

---
+++ Chat/Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+using FireSharp;
+using FireSharp.Config;
+using FireSharp.Extensions;
+using FireSharp.EventStreaming;
+using FireSharp.Interfaces;
+
+
+namespace Chat
+{
+	public class Application
+	{
+		// This is the main entry point of the application.
+		static void Main (string[] args)
+		{
+			IFirebaseConfig config = new FirebaseConfig {
+				AuthSecret = "TsBQjlkc5JaCYuKiuAZ8jMgMz13sKix4xs7f29fx",
+				BasePath = "https://faheemchat.firebaseio.com/"
+			};
+			IFirebaseClient client = new FirebaseClient (config);
+
+			     
+
+			// if you want to use a different Application Delegate class from "AppDelegate"
+			// you can specify it here.
+			UIApplication.Main (args, null, "AppDelegate");
+		}
+	}
+}
